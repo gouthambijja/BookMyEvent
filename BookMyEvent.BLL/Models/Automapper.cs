5a675acba0671fd5b095d0390786e162ff361cf5
venu <venuvk284@gmail.com>
2023-06-08 13:07:52 +0530

    Add Auto Mapper

--- BookMyEvent.BLL/Models/Automapper.cs
+++ BookMyEvent.BLL/Models/Automapper.cs
@@ -1,1 +1,39 @@
+using AutoMapper;
 
+
+
+public class Automapper
+
+{
+
+    public static Mapper InitializeAutomapper()
+
+    {
+
+        var config = new MapperConfiguration(cfg =>
+
+        {
+
+            //cfg.CreateMap<User, UserBL>();
+
+            //cfg.CreateMap<UserBL, User>();
+
+            //cfg.CreateMap<UserBL, LoginUserModel>();
+
+            //cfg.CreateMap<Transaction, TransactionBL>();
+
+            //cfg.CreateMap<TransactionBL, Transaction>();
+
+
+
+         
+
+        });
+
+        var mapper = new Mapper(config);
+
+        return mapper;
+
+    }
+
+}
