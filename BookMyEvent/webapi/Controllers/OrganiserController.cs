6b9fc6188e84a74134e9aeecd7f89ead7a967222
Prabhath Rayabarapu <prabhath.rayabarapu@techigai.io>
2023-06-13 10:39:53 +0530

    Added basic api in organiser controller for testing

--- BookMyEvent/webapi/Controllers/OrganiserController.cs
+++ BookMyEvent/webapi/Controllers/OrganiserController.cs
@@ -1,7 +1,10 @@
 ﻿using BookMyEvent.BLL.Contracts;
 using BookMyEvent.BLL.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+
+
 
 namespace BookMyEvent.WebApi.Controllers
 {
@@ -15,10 +18,41 @@ namespace BookMyEvent.WebApi.Controllers
             _organiserServices = organiserServices;
         }
         [HttpPost("RegisterOrganiser")]
-        public async Task<(bool IsSuccessfull, string Message)> RegisterOrganiser((BLAdministrator profile, BLOrganisation organisation) organiser)
+        public async Task<IActionResult> RegisterOrganiserOwner(Tuple<BLAdministrator, BLOrganisation> organiser)
         {
-            throw new NotImplementedException();
+            //BLAdministrator profile, BLOrganisation organisation
+            //var result = await _organiserServices.RegisterOwner(organiser.profile, organiser.organisation);
+            BLAdministrator administrator = organiser.Item1;
+            BLOrganisation organisation = organiser.Item2;
+            var result = await _organiserServices.RegisterOwner(administrator, organisation);
+            if(result.IsSuccessfull)
+                return Ok(result.Message);
+            else return BadRequest(result.Message);
         }
+
+        [HttpPost("CreateOrganiser")]
+        public async Task<IActionResult> CreateSecondaryOrganiser(BLAdministrator secondaryOwner)
+        {
+            var result = await _organiserServices.CreateSecondaryOwner(secondaryOwner);
+            Console.WriteLine(result.Message);
+            var response = new
+            {
+                IsSuccess = result.IsSuccessfull,
+                Message = result.Message
+            };
+            if (result.IsSuccessfull)
+                return Ok(new
+                {
+                    IsSuccess = result.IsSuccessfull,
+                    Message = result.Message
+                });
+            else return BadRequest(new
+            {
+                IsSuccess = new BLAdministrator(),
+                Message = result.Message
+            });
+        }
+
 
     }
 }
