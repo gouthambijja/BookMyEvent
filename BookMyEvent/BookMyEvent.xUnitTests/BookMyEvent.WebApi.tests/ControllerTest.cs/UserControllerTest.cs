9348477096775fb75dd7be599877c02db9041f0e
Goutham Bijja <goutham.bijja@techigai.io>
2023-07-11 18:08:08 +0530

    Added xUnit Project for UnitTesting.

---
+++ BookMyEvent/BookMyEvent.xUnitTests/BookMyEvent.WebApi.tests/ControllerTest.cs/UserControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyEvent.xUnitTests.BookMyEvent.WebApi.tests.ControllerTest.cs
+{
+    internal class UserControllerTest
+    {
+    }
+}
---
+++ BookMyEvent/BookMyEvent.xUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BookMyEvent.xUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
